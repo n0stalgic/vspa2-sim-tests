0a255ee7f451211cd1ec49da2d4a05038efba7eb
Chris Strong <chris.strong@flocksafety.com>
2026-07-06 12:43:33 -0400

    Delete another swp, switch up some vector arithmetic regs

--- vspa-lib/psk_hw/tests/kernel/psk_hw.c
+++ vspa-lib/psk_hw/tests/kernel/psk_hw.c
@@ -21,20 +21,21 @@ void mod_bpsk_hw(uint32_t* bit_in, vspa_complex_float16* bpsk_out, uint32_t N)
 	__set_prec(single, half_fixed, half_fixed, single, half);
 	__set_Smode(S0i1r1i1r1, S1straight, S2zeros);
 
-	__set_VRAptr_rV(_VR2);
+	__set_VRAptr_rV(_VR0);
 	__mv_w_rV(QAM_NORM_BPSK);
-	__set_VRAptr_rS0(_VR2);
+	__set_VRAptr_rS0(_VR0);
 	__rd_S0();
 
-	__set_VRAptr_rS1(_VR0);
+	__set_VRAptr_rS1(_VR2);
+
 	__rd_S2();
 
-	__set_VRAptr_rSt(2);
+	__set_VRAptr_rSt(0);
 
 	for (uint32_t i = 0; i < N; i++)
 	{
 		__ld_vec(bit_in + i);
-		__ld_Rx(qam, 0);
+		__ld_Rx(qam, 2);
 		__rd_S1();
 		__rmad();
 		__wr(straight);
